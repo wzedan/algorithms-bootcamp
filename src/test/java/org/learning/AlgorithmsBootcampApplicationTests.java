021316f637b88884b8923e85f2d1bd916efc8f96
wz <wzedan@gmail.com>
2017-04-23 09:37:52 +0300

    Initial Version

---
+++ src/test/java/org/learning/AlgorithmsBootcampApplicationTests.java
@@ -0,0 +1,16 @@
+package org.learning;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AlgorithmsBootcampApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
